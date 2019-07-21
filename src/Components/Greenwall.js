7d40f31c0358b01dee3ca7473c2af9595e7580a4
welvin21 <welvin07@gmail.com>
2019-07-22 00:18:14 +0800

    Deploys to gh-pages

---
+++ src/Components/Greenwall.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route, Link, BrowserRouter as Router} from 'react-router-dom';
+// import { Layout} from 'antd';
+//
+// const { Header, Footer, Sider, Content } = Layout;
+
+class Greenwall extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Greenwall</h1>
+                <h1>Greenwall</h1>
+                <h1>Greenwall</h1>
+                <h1>Greenwall</h1>
+                <h1>Greenwall</h1>
+            </div>
+        );
+    }
+}
+
+export default Greenwall;

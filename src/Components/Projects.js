56ab1436f66b5e6ed67cef1e83a343068487bf52
Welvin <wewe2712@connect.hku.hk>
2019-12-29 20:46:31 +0800

    fixed https issue

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,7 +11,7 @@ const projects = [
     pic : pa,
     title : 'Personal Assistant',
     desc : 'Speech recognition powered python script to automate your daily tasks such as checking the weather reports, opening web applications, or surfing through wikipedia.',
-    url : 'http://www.github.com/welvin21/personal_assistant',
+    url : 'https://www.github.com/welvin21/personal_assistant',
     tags : ['python','wikipediaAPI','openweathermapAPI','pyttsx3']
   },
   {
@@ -25,21 +25,21 @@ const projects = [
     pic : sls,
     title : 'Smart Laundry System',
     desc : 'A washing machine tracker application by RCTech Club. Aims to ease the life of R.C. Lee hall residents by providing them the ability to track the status of each washing machines on the 16th floor anywhere, anytime.',
-    url : 'http://sls.rctech.club',
+    url : 'https://sls.rctech.club',
     tags : ['python','javascript','react','AWS','firebase','arduino']
   },
   {
     pic : navis,
     title : 'Navis',
     desc : 'A centralized system to make data communication in the credit insurance escalation process faster.',
-    url : 'http://www.navis-app.co',
+    url : 'https://www.navis-app.co',
     tags : ['javascript','react','express','firebase']
   },
   {
     pic : digibot,
     title : 'Digibot',
     desc : 'Virtual banking assistant focusing on bot interactions to make loan application process easier. Comprises text, voice, and click-based user interactions with well-designed UI.',
-    url : 'http://rebrand.ly/chappuishalder',
+    url : 'https://rebrand.ly/chappuishalder',
     tags : ['javascript','react-native','AWS']
   },
 ];

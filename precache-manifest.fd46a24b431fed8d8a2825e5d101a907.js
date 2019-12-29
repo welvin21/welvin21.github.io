dc00c3091a1a788abf40f8dc107d584505883a1a
Welvin <wewe2712@connect.hku.hk>
2019-12-29 18:15:31 +0800

    first commit

---
+++ precache-manifest.fd46a24b431fed8d8a2825e5d101a907.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a8130d7737e0fcf070f35711085b31",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "19c7c86449da7b2f54cf",
+    "url": "/personal_website/static/css/main.ec3c3fb0.chunk.css"
+  },
+  {
+    "revision": "c5c0f6f76de6927c28e0",
+    "url": "/personal_website/static/js/2.1f3e3074.chunk.js"
+  },
+  {
+    "revision": "19c7c86449da7b2f54cf",
+    "url": "/personal_website/static/js/main.cde6fb12.chunk.js"
+  },
+  {
+    "revision": "9c7ca45164327b004de3",
+    "url": "/personal_website/static/js/runtime~main.5454f9ef.js"
+  },
+  {
+    "revision": "86f6b210adaf9714bfe64e73e9a535b8",
+    "url": "/personal_website/static/media/background1.86f6b210.png"
+  },
+  {
+    "revision": "0e218e77aa6cc73dad4a5a4339715c9b",
+    "url": "/personal_website/static/media/background2.0e218e77.png"
+  },
+  {
+    "revision": "255897a43515a400963a89718d0d39fa",
+    "url": "/personal_website/static/media/digibot.255897a4.png"
+  },
+  {
+    "revision": "cd71a39a2553bfed5b0337e60f600e78",
+    "url": "/personal_website/static/media/greenwall.cd71a39a.png"
+  },
+  {
+    "revision": "04ce06aceffa3c483c2b91d655eaa2fe",
+    "url": "/personal_website/static/media/mypic.04ce06ac.png"
+  },
+  {
+    "revision": "12ed73aec77b7e7735f5e29f19146069",
+    "url": "/personal_website/static/media/navis.12ed73ae.png"
+  },
+  {
+    "revision": "9eb0574b8e98bfebc1f147077a00a4eb",
+    "url": "/personal_website/static/media/personal-assistant.9eb0574b.png"
+  },
+  {
+    "revision": "c1e1fbd5d65fa8d273c128b2deaf88c5",
+    "url": "/personal_website/static/media/sls.c1e1fbd5.png"
+  }
+]);

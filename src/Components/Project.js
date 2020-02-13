34d8a4f2a1d45e95f768be5ac8bb0bc38f966b50
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-02-13 17:07:17 +0800

    Modifies Project card
    * Adds buttons to project card component
    
    * Changes README.md

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,28 +1,53 @@
 import React from 'react';
-import { Tag } from 'antd';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import { Tag, notification } from 'antd';
+import 'antd/dist/antd.css'; 
+
+const useStyles = makeStyles( theme => ({
+  picture: {
+    width: '100%',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5
+  },
+  buttons: {
+    marginBottom: 10,
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    gridGap: 10
+  }
+}));
 
 const identifyTag = tag => {
   const languages = ['python','javascript','java','c++'];
   const libraries = ['react','react-native','express','redux'];
   const clouds = ['firebase','AWS','azure'];
 
-  let color = '#f50';
+  let color = 'magenta';
   if(languages.includes(tag)){
-    color = '#2db7f5';
+    color = 'blue';
   }else if(libraries.includes(tag) || tag.toLowerCase().includes('api')){
-    color = '#87d068';
+    color = 'green';
   }else if(clouds.includes(tag)){
-    color = '#108ee9';
+    color = 'orange';
   }
   return <Tag key={tag} className='tag' color={color}>{tag}</Tag>;
+};
+
+const openMessage = () => {
+  notification.error({
+    message: 'Code is not available for this project.',
+    description: 'Please kindly check my other projects!'
+  });
 }
 
-const Project = props => {
-  const { project : {pic, title, desc, url,tags}} = props;
+const Project = ({ project : {pic, title, desc, url, github, tags}}) => {
+  const classes = useStyles();
   return(
-    <div className='project-card' onClick={() => {window.open(url,'_blank','noopener')}}>
+    <div className='project-card'>
       <div>
-        <img style={{width: '100%', borderTopLeftRadius: 5, borderTopRightRadius: 5}} src={pic} alt={title}/>
+        <img className={classes.picture} src={pic} alt={title}/>
       </div>
       <div style={{padding: '0px 10px'}}>
         <div style={{paddingTop: 10}}>
@@ -30,6 +55,28 @@ const Project = props => {
         </div>
         <h2 style={{color: '#4d4d4d',marginBottom: 0}}>{title}</h2>
         <p style={{paddingBottom: 10,marginTop: 5}}>{desc}</p>
+        <div className={classes.buttons}>
+          <Button 
+            variant='outlined' 
+            color="primary" 
+            size='small'
+            disableElevation 
+            style={{fontSize: 'min(100%, 16px)'}}
+            onClick={() => {window.open(url,'_blank','noopener')}}
+          >
+            See more
+          </Button>
+          <Button 
+            variant='outlined' 
+            size='small'
+            disableElevation
+            startIcon={<GitHubIcon/>}
+            style={{fontSize: 'min(100%, 16px)'}}
+            onClick={() => { github ? window.open(github,'_blank','noopener') : openMessage() }}
+          >
+            Code
+          </Button>
+        </div>
       </div>
     </div>
   );

015eebd0da039e815fd5cbe2da618744fe5328f2
welvin21 <wewe2712@connect.hku.hk>
2020-02-14 18:37:44 +0800

    Removes loader due to test issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Footer from './Components/Footer';
-import { Loader } from './Components/Loader';
 import './App.css';
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
-  return isLoading ? (
-    <Loader />
-  ) : (
+  return (
     <>
       <Header />
       <Home />

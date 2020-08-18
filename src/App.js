c892ce37f73d4722ced873cf71e0fbc7584c4888
welvin21 <wewe2712@connect.hku.hk>
2020-08-18 10:22:04 +0800

    Remove home section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Header } from './Components/Header';
-import { Home } from './Components/Home';
 import { About } from './Components/About';
 import { Projects } from './Components/Projects';
 import { Experiences } from './Components/Experiences';
@@ -11,7 +10,6 @@ export const App = () => {
   return (
     <>
       <Header />
-      <Home />
       <About />
       <Projects />
       <Experiences />

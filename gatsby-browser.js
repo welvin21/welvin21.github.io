74de25f370a66bc99c3ef3ea9af7ae74b0cee663
welvin21 <wewe2712@connect.hku.hk>
2021-01-10 15:07:18 +0800

    Provide wrapRootElement gatsby API for initial theming when pages first loaded

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,12 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from 'react';
+import { ThemeProvider, ColorModeProvider, CSSReset } from '@chakra-ui/core';
+import theme from './src/gatsby-plugin-chakra-ui/theme';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider>
+      <CSSReset />
+      {element}
+    </ColorModeProvider>
+  </ThemeProvider>
+);
